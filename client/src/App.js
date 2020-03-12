79a869603074caebb90c7c3b1dbd41cb19098d18
Aleksey Belchenkov <u608110@gmail.com>
2020-03-12 14:09:23 +0300

    Protected Route For Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Styles
+import './App.css';
+
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
 
-import './App.css';
 
+// Components
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
-import setAuthToken from "./utils/setAuthToken";
-import { loadUser } from "./actions/auth";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 if (localStorage.token) {
     setAuthToken(localStorage.token);
@@ -35,6 +40,7 @@ const App = () => {
                         <Switch>
                             <Route exact path="/register" component={Register} />
                             <Route exact path="/login" component={Login} />
+                            <PrivateRoute exact path="/dashboard" component={Dashboard} />
                         </Switch>
                     </section>
                 </Fragment>
