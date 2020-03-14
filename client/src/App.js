cab055b7484187ea2881bcd1c12d2065c1d3a8db
Aleksey Belchenkov <u608110@gmail.com>
2020-03-14 18:28:57 +0300

    Edit Profile

--- client/src/App.js
+++ client/src/App.js
@@ -14,12 +14,13 @@ import { loadUser } from "./actions/auth";
 // Components
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
-import PrivateRoute from "./components/routing/PrivateRoute";
+import EditProfile from "./components/profile-forms/EditProfile";
 
 if (localStorage.token) {
     setAuthToken(localStorage.token);
@@ -43,6 +44,7 @@ const App = () => {
                             <Route exact path="/login" component={Login} />
                             <PrivateRoute exact path="/dashboard" component={Dashboard} />
                             <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                            <PrivateRoute exact path="/edit-profile" component={EditProfile} />
                         </Switch>
                     </section>
                 </Fragment>
