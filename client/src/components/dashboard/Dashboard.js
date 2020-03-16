5ba4b7041b8ef50dccc16ea601ac65d9002204bc
Aleksey Belchenkov <u608110@gmail.com>
2020-03-16 14:31:06 +0300

    Delete Education Experiences Account

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { getCurrentProfile } from '../../actions/profile';
+import { deleteAccount, getCurrentProfile } from '../../actions/profile';
 import Spinner from "../layout/Spinner";
 import DashboardActions from "./DashboardActions";
 import Experience from "./Experience";
@@ -11,6 +11,7 @@ import Education from "./Education";
 
 const Dashboard = ({
                        getCurrentProfile,
+                       deleteAccount,
                        auth: { user },
                        profile: { profile, loading }
 }) => {
@@ -28,6 +29,13 @@ const Dashboard = ({
                     <DashboardActions />
                     <Experience experience={profile.experience} />
                     <Education education={profile.education} />
+
+                    <div className="my-2">
+                        <button className="btn btn-danger" onClick={() => deleteAccount()}>
+                            <i className="fas fa-user-minus" /> Delete My Account
+                        </button>
+                    </div>
+
                 </Fragment>
                 : (<Fragment>
                     <p>You have not yet setup a profile, please add some info</p>
@@ -40,6 +48,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 };
@@ -49,4 +58,4 @@ const mapStateToProps = state => ({
     profile: state.profile
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
