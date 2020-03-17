648cc7c0187cb940144f9c07e8f01ffab34494d1
Aleksey Belchenkov <u608110@gmail.com>
2020-03-17 16:48:25 +0300

    Display Profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,6 +9,9 @@ const Navbar = ({ auth: { isAuthenticated, loading } }) => {
     const authLinks = (
         <ul>
             <li>
+                <Link to="/profiles">Developers</Link>
+            </li>
+            <li>
                 <Link to="/dashboard">
                     <i className="fas fa-user" /> {' '}
                     <span className="hide-sm">Dashboard</span>
@@ -23,7 +26,7 @@ const Navbar = ({ auth: { isAuthenticated, loading } }) => {
 
     const guestLinks = (
         <ul>
-            <li><a href="!#">Developers</a></li>
+            <li><Link to="/profiles">Developers</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/login">Login</Link></li>
         </ul>
