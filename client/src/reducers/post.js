d42952d923588b8996fb5a18dce061ccfb15d3b5
Aleksey Belchenkov <u608110@gmail.com>
2020-03-20 11:35:47 +0300

    Adding Comments

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,7 +4,8 @@ import {
     POST_ERROR,
     DELETE_POST,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT, REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -52,6 +53,21 @@ export default function (state= initialState, action) {
             return {
                 ...state,
                 posts: [...state.posts, payload],
+                loading: false
+            };
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: {...state.post, comments: payload},
+                loading: false
+            };
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(comment => comment._id === payload)
+                },
                 loading: false
             };
         default:
